52b8c5f9267fa9216964dab07303f82a9344dba2
LiamPenny <62670550+LiamPenny@users.noreply.github.com>
2022-03-06 08:50:06 +1300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Title = "C#Helper";
+            Console.WriteLine("C#Helper");
+            Console.WriteLine("Welcome to my first program in C#!");
+            Console.WriteLine("                       ");
+            Console.WriteLine("                       ");
+            Console.WriteLine("                       ");
+            Console.WriteLine("                       ");
+            Console.WriteLine("Hello what's your name?");
+            Console.ReadLine();
+            Console.WriteLine("Hello there");
+            Console.WriteLine("My name is 649XR. I'm an AI sent from the universe of Microsoft.");
+            Console.WriteLine("It's nice to meet you, we have a lot to learn about C#.");
+            Console.WriteLine("You can type your happyness underhere");
+            Console.ReadLine();
+            Console.WriteLine("So the first thing you need to have is a code editer such as VSCode or notepad++. Once you have that we are ready to start!");
+            Console.WriteLine("Alright so what you want to do is create a folder on your desktop and then click open folder in VSCode"); 
+            Console.WriteLine("Now click open folder. This will create the files we need.");
+            Console.ReadKey();
